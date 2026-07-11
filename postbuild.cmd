50384134d601ba8c02a52a404371268528d54e02
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-11 07:52:58 +0300

    building scripts have been updated

--- postbuild.cmd
+++ postbuild.cmd
@@ -3,4 +3,4 @@ rd /s /q c:\!Development\!Projects\Prod-Server-3A\dist\public\apps\ssngrx
 
 mkdir c:\!Development\!Projects\Prod-Server-3A\dist\public\apps\ssngrx
 
-xcopy /s /y c:\!Development\!Projects\Auth-Server-Frontend\dist\standalone-signals\browser c:\!Development\!Projects\Prod-Server-3A\dist\public\apps\ssngrx
+xcopy /s /y c:\!Development\!Projects\Auth-Server-Frontend\dist\browser c:\!Development\!Projects\Prod-Server-3A\dist\public\apps\ssngrx
